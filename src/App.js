0d43883113e8935488a18a78dfde0e7272f9b586
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-16 12:55:32 +0100

    Add scripts and tempalte

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      The list of comments!
+    </div>
+  );
+};
+
+export default App;
---
+++ src/users.js
@@ -0,0 +1,7 @@
+//reducer users
+
+function users(state = [], action) {
+	return state;
+}
+
+export default users;
